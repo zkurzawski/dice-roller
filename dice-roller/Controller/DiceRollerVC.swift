edd93cd3596fa7878bdd51d980ad5e35d3f2bc0c
Zakary Kurzawski <zakarykurzawski@Zakarys-MacBook-Pro.local>
2018-03-17 08:49:40 -0500

    got rid of unused options elements. laid out new options elements

--- dice-roller/Controller/DiceRollerVC.swift
+++ dice-roller/Controller/DiceRollerVC.swift
@@ -12,31 +12,28 @@ class DiceRollerVC: UIViewController, UICollectionViewDelegate, UICollectionView
 
     @IBOutlet weak var viewConstraint: NSLayoutConstraint!
    
-    @IBOutlet weak var diceSliderValueLbl: UILabel!
+    @IBOutlet weak var d4QtyLbl: UILabel!
+    @IBOutlet weak var d6QtyLbl: UILabel!
+    @IBOutlet weak var d8QtyLbl: UILabel!
+    @IBOutlet weak var d10QtyLbl: UILabel!
+    @IBOutlet weak var d12QtyLbl: UILabel!
+    @IBOutlet weak var d20QtyLbl: UILabel!
+    @IBOutlet weak var d100QtyLbl: UILabel!
+    
+    @IBOutlet weak var d6DDImg: UIImageView!
+    @IBOutlet weak var d6TradImg: UIImageView!
+    
     @IBOutlet weak var diceSizeLbl: UILabel!
     @IBOutlet weak var diceQtyLbl: UILabel!
-    @IBOutlet weak var diceChoiceLbl00: UILabel!
-    @IBOutlet weak var diceChoiceLbl01: UILabel!
-    @IBOutlet weak var diceChoiceLbl02: UILabel!
-    @IBOutlet weak var diceChoiceLbl03: UILabel!
-    @IBOutlet weak var diceChoiceLbl04:UILabel!
-    @IBOutlet weak var diceChoiceLbl05:UILabel!
-    @IBOutlet weak var diceChoiceLbl06:UILabel!
-    
-    
-    @IBOutlet weak var d6SelectionLbl: SixSidedLabel!
-    @IBOutlet weak var traditionalSelectedLbl: SixSidedLabel!
-    
-    @IBOutlet weak var diceTypeImage: UIImageView!
-    @IBOutlet weak var d6StackView: UIStackView!
     
     @IBOutlet weak var diceCollectionView: UICollectionView!
     
 //    stepper test
     
-    @IBOutlet weak var diceQtyStepper: UIStepper!
-    @IBOutlet weak var stepperLbl: UILabel!
     var stepperQty = 0
+    
+    var diceQtyArr = [0.0,0.0,3.0,0.0,0.0,0.0,0.0]
+    var resetValue = false
 //
     
     var diceQty = 1
@@ -55,13 +52,14 @@ class DiceRollerVC: UIViewController, UICollectionViewDelegate, UICollectionView
         diceCollectionView.delegate = self
         diceCollectionView.dataSource = self
     
-        diceTypeQtyLbls = [diceChoiceLbl00, diceChoiceLbl01, diceChoiceLbl02, diceChoiceLbl03, diceChoiceLbl04, diceChoiceLbl05, diceChoiceLbl06]
+       /* diceTypeQtyLbls = [diceChoiceLbl00, diceChoiceLbl01, diceChoiceLbl02, diceChoiceLbl03, diceChoiceLbl04, diceChoiceLbl05, diceChoiceLbl06]
         
         super.viewDidLoad()
         d6SelectionLbl.selected()
         traditionalSelectedLbl.deselected()
         d6StackView.isHidden = false
         diceTypeImage.image = UIImage(named: "d6_img5.png")
+ */
         
         viewConstraint.constant = -340
        
@@ -96,34 +94,39 @@ class DiceRollerVC: UIViewController, UICollectionViewDelegate, UICollectionView
         panActivated(viewConstraint: viewConstraint, panInfo: sender)
     }
    
-    @IBAction func diceSliderChange(_ sender: UISlider) {
-        diceQty = Int(sender.value)
-        OptionsResults.instance.sliderResults(diceTypeLbl: diceSliderValueLbl, diceQtyLbl: diceQtyLbl, diceQty: diceQty, upperDiceTypeLbl: diceTypeQtyLbls, diceSize: segmentedSelectionResult)
-    }
-   
-    @IBAction func diceSelectionSegCntrl(_ sender: UISegmentedControl) {
-        diceSize = diceSizeSelect(selected: sender.selectedSegmentIndex)
-        segmentedSelectionResult = sender.selectedSegmentIndex
-        print("dice size selected: \(segmentedSelectionResult)")
-        OptionsResults.instance.segmentedCntrlResults(d6Stack: d6StackView, diceTypeImg: diceTypeImage, diceSizeLbl: diceSizeLbl, diceSize: diceSize, segmentedChoice: sender)
-    }
     
-    @IBAction func sixSidedTypeChose(_ sender: UISwitch) {
+  /*  @IBAction func sixSidedTypeChose(_ sender: UISwitch) {
         d6Choice = sender.isOn
         OptionsResults.instance.sixSidedTypeChoose(d6TypeLbl: d6SelectionLbl, tradTypeLbl: traditionalSelectedLbl, d6TradImage: diceTypeImage, switchResult: sender)
-    }
+    } */
     
     @IBAction func rollBtn(_ sender: UIButton) {
         dice = DiceArrayFill.instance.arrayFill(diceSize: diceSize, diceQty: diceQty, d6Choice: d6Choice)
         diceCollectionView.reloadData()
     }
     
-//    stepper test actions
+/*    stepper test actions
     @IBAction func qtyStep(_ sender: UIStepper) {
+        
+       /******************************************
+        ******************************************
+        Some indicator that the step has been made
+        so the value can increment/decrement. As
+        it is, the code sees that the value isn't 0
+        and refuses to change it from the if
+        statement.
+        *******************************************
+        ******************************************/
+        
+        if (diceQtyArr[2] > 0) && (resetValue == false) {
+            sender.value = diceQtyArr[2]
+            resetValue = true
+        }
         stepperQty = Int(sender.value)
 //        stepperLbl.text = "\(Int(sender.value))"
         stepperLbl.text = "\(stepperQty)"
+        
     }
-//
+*/
     
 }
